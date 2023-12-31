2988d2f80a7e4d882a3b629f2d65027a5124635e
Youssef Belhadj <56652008+usfbelhadj@users.noreply.github.com>
2023-12-31 16:38:57 +0100

    Update AppConstants.dart

--- cleanch/lib/map/AppConstants.dart
+++ cleanch/lib/map/AppConstants.dart
@@ -2,9 +2,9 @@ import 'package:latlong2/latlong.dart';
 
 class AppConstants {
   static const String mapBoxAccessToken =
-      'pk.eyJ1IjoiZGpvMDEiLCJhIjoiY2xrN2c0bjNjMDh0azNncnRzOXpnZ3ZpOSJ9.0Xql4ABT60WUcpKac_DUpA';
+      env.token;
 
-  static const String mapBoxStyleId = 'djo01/clhw7uqcg024x01quakk60mdi';
+  static const String mapBoxStyleId = env.mapStyle;
 
   static final myLocation = LatLng(51.5090214, -0.1982948);
 }
